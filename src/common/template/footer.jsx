9feb3ed1c3ce01b682cb3f108455d0acd3a363ea
zYumoto <victorjuanrazer@gmail.com>
2022-11-09 15:25:22 -0300

    add: component header/footer

---
+++ src/common/template/footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default (props) => (
+  <footer className="main-footer">
+    <strong>
+      Copyright &copy; 2022
+      <a href="https://github.com/zYumoto" target="_blank">
+        Victor Yumoto
+      </a>
+    </strong>
+  </footer>
+);
