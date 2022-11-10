f9646c7c6220d5662207cd0d85c2a86d625c1358
zYumoto <victorjuanrazer@gmail.com>
2022-11-10 17:14:34 -0300

    add: Action primary o redux

--- src/main/reducers.js
+++ src/main/reducers.js
@@ -1,9 +1,12 @@
 import { combineReducers } from "redux";
 import DashboardReducer from "../dashboard/dashboardReducer";
+import tabReducer from "../common/tab/tabReducer";
 
 
 const rootReducer = combineReducers({
     dashboard: DashboardReducer,
+    tab: tabReducer,
+
 });
 
 export default rootReducer;
